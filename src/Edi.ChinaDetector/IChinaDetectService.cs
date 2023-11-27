e51099311b36ee2cee85e3531b745c45c59b3ba1
Edi Wang <Edi.Wang@outlook.com>
2023-11-27 13:00:39 +0800

    Create IChinaDetectService.cs

---
+++ src/Edi.ChinaDetector/IChinaDetectService.cs
@@ -0,0 +1,22 @@
+﻿namespace Edi.ChinaDetector;
+
+public interface IChinaDetectService
+{
+    public Task<ChinaDetectResult> Detect(DetectionMethod method);
+}
+
+[Flags]
+public enum DetectionMethod : int
+{
+    TimeZone,
+    Culture,
+    IPAddress,
+    GFWTest
+}
+
+public class ChinaDetectResult
+{
+    public int Rank { get; set; }
+    public DetectionMethod PositiveMethod { get; set; }
+    public string IPAddress { get; set; }
+}
