f51183748f80a381c41b2aae96d16924e1e76095
Edi Wang <Edi.Wang@outlook.com>
2023-11-28 12:56:50 +0800

    Update AllTests.cs

--- src/Edi.ChinaDetector.Tests/AllTests.cs
+++ src/Edi.ChinaDetector.Tests/AllTests.cs
@@ -41,7 +41,7 @@ public class AllTests
     }
 
     [Test]
-    public async Task DetectCultureOnePositive()
+    public async Task DetectCulturePositive()
     {
         var httpClient = new HttpClient();
         var service = new ChinaDetectService(httpClient);
@@ -53,5 +53,26 @@ public class AllTests
         });
 
         Assert.That(result.Rank, Is.EqualTo(1));
+        Assert.That(result.PositiveMethods, Is.Not.Null);
+        Assert.That(result.PositiveMethods.Count == 1, Is.True);
+        Assert.That(result.PositiveMethods.First(), Is.EqualTo(DetectionMethod.Culture));
+    }
+
+    [Test]
+    public async Task DetectCultureBothPositive()
+    {
+        var httpClient = new HttpClient();
+        var service = new ChinaDetectService(httpClient);
+
+        var result = await service.Detect(DetectionMethod.Culture, new()
+        {
+            TargetCulture = CultureInfo.GetCultureInfo("zh-CN"),
+            TargetUICulture = CultureInfo.GetCultureInfo("zh-CN")
+        });
+
+        Assert.That(result.Rank, Is.EqualTo(2));
+        Assert.That(result.PositiveMethods, Is.Not.Null);
+        Assert.That(result.PositiveMethods.Count == 1, Is.True);
+        Assert.That(result.PositiveMethods.First(), Is.EqualTo(DetectionMethod.Culture));
     }
 }
