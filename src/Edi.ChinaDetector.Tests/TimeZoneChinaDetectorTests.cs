16af672e0a6ca7cf9ac4987204ce1be9cba95459
Edi Wang <Edi.Wang@outlook.com>
2023-11-28 13:13:24 +0800

    Create TimeZoneChinaDetectorTests.cs

---
+++ src/Edi.ChinaDetector.Tests/TimeZoneChinaDetectorTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Edi.ChinaDetector.Tests;
+
+[TestFixture]
+public class TimeZoneChinaDetectorTests
+{
+    [Test]
+    public void DetectPositive()
+    {
+        var detector = new TimeZoneChinaDetector(TimeZoneInfo.FindSystemTimeZoneById("China Standard Time"));
+
+        var result = detector.Detect();
+
+        Assert.That(result, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void DetectNegative()
+    {
+        var detector = new TimeZoneChinaDetector(TimeZoneInfo.FindSystemTimeZoneById("India Standard Time"));
+
+        var result = detector.Detect();
+
+        Assert.That(result, Is.EqualTo(0));
+    }
+}
