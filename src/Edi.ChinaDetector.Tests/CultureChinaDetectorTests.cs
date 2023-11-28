0e8a3ce6c48bbfd5ce2dacfa7aefdf8fecefd16b
Edi Wang <Edi.Wang@outlook.com>
2023-11-28 13:22:47 +0800

    Create CultureChinaDetectorTests.cs

---
+++ src/Edi.ChinaDetector.Tests/CultureChinaDetectorTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using NUnit.Framework;
+
+namespace Edi.ChinaDetector.Tests;
+
+[TestFixture]
+public class CultureChinaDetectorTests
+{
+    [Test]
+    public void DetectPositiveCulture()
+    {
+        var cul = CultureInfo.GetCultureInfo("zh-CN");
+        var uiCul = CultureInfo.GetCultureInfo("en-US");
+
+        var detector = new CultureChinaDetector(cul, uiCul);
+
+        var result = detector.Detect();
+
+        Assert.That(result, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void DetectPositiveBoth()
+    {
+        var cul = CultureInfo.GetCultureInfo("zh-CN");
+        var uiCul = CultureInfo.GetCultureInfo("zh-CN");
+
+        var detector = new CultureChinaDetector(cul, uiCul);
+
+        var result = detector.Detect();
+
+        Assert.That(result, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void DetectPositiveUICulture()
+    {
+        var cul = CultureInfo.GetCultureInfo("en-US");
+        var uiCul = CultureInfo.GetCultureInfo("zh-CN");
+
+        var detector = new CultureChinaDetector(cul, uiCul);
+
+        var result = detector.Detect();
+
+        Assert.That(result, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void DetectNegative()
+    {
+        var cul = CultureInfo.GetCultureInfo("en-US");
+        var uiCul = CultureInfo.GetCultureInfo("en-US");
+
+        var detector = new CultureChinaDetector(cul, uiCul);
+
+        var result = detector.Detect();
+
+        Assert.That(result, Is.EqualTo(0));
+    }
+}
