bcb55d627bc3e829eab0d9c25dd10105b6162715
Edi Wang <Edi.Wang@outlook.com>
2023-11-28 13:11:36 +0800

    code refact

--- src/Edi.ChinaDetector/OfflineChinaDetectService.cs
+++ src/Edi.ChinaDetector/OfflineChinaDetectService.cs
@@ -38,18 +38,7 @@ public class OfflineChinaDetectService : IChinaDetectService
         return Task.FromResult(result);
     }
 
-    private static int DetectByTimeZone(TimeZoneInfo timeZone)
-    {
-        timeZone ??= TimeZoneInfo.Local;
-
-        if (timeZone.Id == "China Standard Time" ||
-            timeZone.StandardName.Contains("china", StringComparison.CurrentCultureIgnoreCase))
-        {
-            return 1;
-        }
-
-        return 0;
-    }
+    private static int DetectByTimeZone(TimeZoneInfo timeZone) => new TimeZoneChinaDetector(timeZone).Detect();
 
     private static int DetectByCulture(CultureInfo culture = null, CultureInfo uiCulture = null)
     {
---
+++ src/Edi.ChinaDetector/TimeZoneChinaDetector.cs
@@ -0,0 +1,17 @@
+﻿namespace Edi.ChinaDetector;
+
+public class TimeZoneChinaDetector(TimeZoneInfo timeZone)
+{
+    public int Detect()
+    {
+        timeZone ??= TimeZoneInfo.Local;
+
+        if (timeZone.Id == "China Standard Time" ||
+            timeZone.StandardName.Contains("china", StringComparison.CurrentCultureIgnoreCase))
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+}
