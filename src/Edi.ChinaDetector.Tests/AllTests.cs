3a8d3be63b1b25ffa9c8cd81b9d7bf4110f40271
Edi Wang <Edi.Wang@outlook.com>
2023-11-28 12:37:54 +0800

    UT - timezone

---
+++ src/Edi.ChinaDetector.Tests/AllTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace Edi.ChinaDetector.Tests;
+
+[TestFixture]
+public class AllTests
+{
+    [Test]
+    public async Task DetectTimeZonePositive()
+    {
+        var httpClient = new HttpClient();
+        var service = new ChinaDetectService(httpClient);
+
+        var result = await service.Detect(DetectionMethod.Culture, new()
+        {
+            //TargetCulture = CultureInfo.GetCultureInfo("zh-CN"),
+            //TargetUICulture = CultureInfo.GetCultureInfo("zh-CN"),
+            TargetTimeZone = TimeZoneInfo.FindSystemTimeZoneById("China Standard Time")
+        });
+
+        Assert.That(result.Rank, Is.EqualTo(1));
+        Assert.That(result.PositiveMethod, Is.EqualTo(DetectionMethod.Culture));
+    }
+
+    [Test]
+    public async Task DetectTimeZoneNegative()
+    {
+        var httpClient = new HttpClient();
+        var service = new ChinaDetectService(httpClient);
+
+        var result = await service.Detect(DetectionMethod.Culture, new()
+        {
+            TargetTimeZone = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time")
+        });
+
+        Assert.That(result.Rank, Is.EqualTo(0));
+        Assert.That(result.PositiveMethod, Is.Null);
+    }
+}
