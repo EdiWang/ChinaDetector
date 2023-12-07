54f391f3bc51a4eb57048e0b801d31806393caa1
Edi Wang <Edi.Wang@outlook.com>
2023-12-07 11:08:43 +0800

    Update TimeZoneChinaDetector.cs

--- src/Edi.ChinaDetector/TimeZoneChinaDetector.cs
+++ src/Edi.ChinaDetector/TimeZoneChinaDetector.cs
@@ -6,6 +6,16 @@ public class TimeZoneChinaDetector(TimeZoneInfo timeZone)
     {
         timeZone ??= TimeZoneInfo.Local;
 
+        /* Not all regions in the +8 time zone use the name "China Standard Time" (CST).
+           For example, the +8 time zone also includes the following time zone names:
+           
+           Australian Western Standard Time (AWST)
+           Singapore Time (SGT)
+           Hong Kong Time (HKT)
+           Malaysia Time (MYT)
+           Philippine Time (PHT)
+           Western Indonesian Time (WIB) */
+
         if (timeZone.Id == "China Standard Time" ||
             timeZone.StandardName.Contains("china", StringComparison.CurrentCultureIgnoreCase))
         {
